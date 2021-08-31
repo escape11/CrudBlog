224692b53e3ccaa63bc1ed64273b9c82dca732ca
escape11 <nikky.sqe@gmail.com>
2021-08-30 20:02:57 -0400

    menu update

--- routes/about.js
+++ routes/about.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     
-  var username = 'Jide'//select username from the table
+  //select username from the table
   res.render('about', { title: 'About Us'});
 });
 
